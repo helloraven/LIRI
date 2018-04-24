c509e23d0b9ae838643860e654446cdf5f04fb32
Raven Bell <raven.bell@yahoo.com>
2018-04-24 18:02:37 -0400

    added spotify keys

--- liri.js
+++ liri.js
@@ -5,9 +5,13 @@ var keys = require('./keys.js');
 
 var Twitter = require('twitter');
 
+var Spotify = require('node-spotify-api');
+
 var client = new Twitter(keys.twitter);
 
-var params = {
+var fs = require('fs');
+
+params = {
     screen_name: "raven_codes",
     count: 5
 };
@@ -27,6 +31,44 @@ var latestTweets = function () {
 
 }
 
+var getArtistName = function(artist) {
+    return artist.name;
+  }
+  
+  
+  var song = function(songName) {
+    var spotify = new Spotify(keys.spotify);
+    spotify.search({
+        type: 'track',
+        query: songName,
+      },
+      function(err, data) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        var songs = data.tracks.items;
+        for (var i = 0; i < songs.length; i++) {
+          console.log(i);
+          // console.log(songs[i])
+          console.log('artist:' + songs[i].artists.map(function(artist) {
+            return artist.name
+          }))
+          console.log('song name:' + songs[i].name);
+          console.log('album name:' + songs[i].album.name);
+          console.log('preview song:' + songs[i].preview_url);
+          console.log('---------------------');
+  
+        }
+      });
+  }
+ 
+ /* var run = function(arg1, arg2) {
+  choose(arg1, arg2);
+ 
+ };
+ run(process.argv[2], process.argv[3]);
+
 var choose = function (command, functionData) {
     switch (command) {
         case 'my-latest-tweets':
@@ -40,4 +82,4 @@ var choose = function (command, functionData) {
 var run = function (arg1, arg2) {
     choose(arg1, arg2);
 };
-run(process.argv[2], process.argv[3]);
+run(process.argv[2], process.argv[3]); */
